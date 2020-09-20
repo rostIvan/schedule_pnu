5b48a8c339ac782fd522882e9493975f0ea583bf
Rostislav Ivankiv <rostyslav.ivankiv@gmail.com>
2020-09-20 21:59:05 +0300

    [Fix] runtime assertion '!_debugLocked'

--- lib/page/selection/view.dart
+++ lib/page/selection/view.dart
@@ -7,7 +7,6 @@ import 'package:lessons_schedule_pnu/page/selection/state.dart';
 import 'package:lessons_schedule_pnu/util/support.dart';
 import 'package:redux/redux.dart';
 
-
 class SelectionPage extends StatefulWidget {
   final Store<SearchState> store;
   final SearchInteractor interactor;
@@ -17,7 +16,8 @@ class SelectionPage extends StatefulWidget {
   SelectionPage(this.store, this.interactor, {Key key}) : super(key: key);
 
   @override
-  SelectionPageState createState() => SelectionPageState(store, interactor, _scaffoldKey);
+  SelectionPageState createState() =>
+      SelectionPageState(store, interactor, _scaffoldKey);
 }
 
 class SelectionPageState extends State<SelectionPage> {
@@ -29,7 +29,6 @@ class SelectionPageState extends State<SelectionPage> {
   final textController = TextEditingController();
   final appBarSearchTypeInfo = SearchTypeInfo();
 
-
   SelectionPageState(this.store, this.interactor, this._scaffoldKey);
 
   @override
@@ -40,49 +39,60 @@ class SelectionPageState extends State<SelectionPage> {
 
   @override
   Widget build(BuildContext context) {
-    var scaffold = Scaffold(key: _scaffoldKey, appBar: _buildAppBar(), body: _buildBody(), floatingActionButton: _buildFab());
+    var scaffold = Scaffold(
+        key: _scaffoldKey,
+        appBar: _buildAppBar(),
+        body: _buildBody(),
+        floatingActionButton: _buildFab());
     return StoreProvider<SearchState>(store: store, child: scaffold);
   }
 
   AppBar _buildAppBar() => AppBar(
-    title: _appBarTitle(),
-    actions: <Widget>[ _selectButton() ],
-  );
+        title: _appBarTitle(),
+        actions: <Widget>[_selectButton()],
+      );
 
-  Widget _appBarTitle() => Row(children: <Widget>[ Text('Пошук '), appBarSearchTypeInfo ]);
+  Widget _appBarTitle() =>
+      Row(children: <Widget>[Text('Пошук '), appBarSearchTypeInfo]);
 
-  Widget  _selectButton() => StoreConnector<SearchState, SearchState>(
+  Widget _selectButton() => StoreConnector<SearchState, SearchState>(
       converter: (store) => store.state,
-      builder: (context, state) => state.selectedItem == null ? Container() :
-      IconButton(icon: Icon(Icons.check, color: Colors.white), onPressed: () => interactor.selectItem(state.selectedItem))
-  );
+      builder: (context, state) => state.selectedItem == null
+          ? Container()
+          : IconButton(
+              icon: Icon(Icons.check, color: Colors.white),
+              onPressed: () => interactor.selectItem(state.selectedItem)));
 
-  Widget _buildBody() => Column(children: <Widget>[SizedBox(height: 4.0), _buildInput(),  _buildSearchArea()]);
+  Widget _buildBody() => Column(children: <Widget>[
+        SizedBox(height: 4.0),
+        _buildInput(),
+        _buildSearchArea()
+      ]);
 
   Widget _buildFab() => StoreConnector<SearchState, ScheduleType>(
       converter: (store) => store.state.searchType,
       builder: (context, searchType) => FloatingActionButton(
           onPressed: () => interactor.changeSearchType(),
-          child: searchType == ScheduleType.GROUP ? Icon(Icons.person) : Icon(Icons.group)
-      )
-  );
+          child: searchType == ScheduleType.GROUP
+              ? Icon(Icons.person)
+              : Icon(Icons.group)));
 
-  Widget _buildInput() => SearchTextField(
-      controller: textController,
-      interactor: interactor
-  );
+  Widget _buildInput() =>
+      SearchTextField(controller: textController, interactor: interactor);
 
   Widget _buildSearchArea() => StoreConnector<SearchState, SearchState>(
       converter: (store) => store.state,
-      builder: (context, state) => state.isSearching ? _progress() : _suggestions(state.results)
-  );
+      builder: (context, state) =>
+          state.isSearching ? _progress() : _suggestions(state.results));
 
   Widget _progress() => Progress();
 
-  Widget _suggestions(List<String> suggestions) => SuggestionListView(suggestions, interactor);
+  Widget _suggestions(List<String> suggestions) =>
+      SuggestionListView(suggestions, interactor);
 
   void showMessage(message) {
-    _scaffoldKey.currentState.showSnackBar(new SnackBar(content: new Text(message)));
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(message)));
   }
 
   void hideMessage() {
@@ -90,10 +100,8 @@ class SelectionPageState extends State<SelectionPage> {
   }
 
   void showHomePage(SelectedData data, String message) {
-    Navigator.of(_scaffoldKey.currentContext).pop();
-    Navigator.of(_scaffoldKey.currentContext).pushReplacement(
-        MaterialPageRoute(builder: (context) => HomePage(data: data, startMessage: message))
-    );
+    Navigator.of(_scaffoldKey.currentContext).pushReplacement(MaterialPageRoute(
+        builder: (context) => HomePage(data: data, startMessage: message)));
   }
 }
 
@@ -104,60 +112,79 @@ class SearchTextField extends StatelessWidget {
   SearchTextField({Key key, this.interactor, this.controller});
 
   @override
-  Widget build(BuildContext context) => StoreConnector<SearchState, ScheduleType> (
+  Widget build(BuildContext context) =>
+      StoreConnector<SearchState, ScheduleType>(
           converter: (store) => store.state.searchType,
-          builder: (context, searchType) =>
-              TextField(
-                  controller: controller,
-                  onChanged: (text) => interactor.search(text),
-                  style: TextStyle(fontSize: 18.0, color: Colors.black),
-                  autofocus: true,
-                  decoration: InputDecoration(
-                      hintText: _hint(searchType),
-                      suffix: CircleIconButton(onPressed: () => interactor.clearText()),
-                      contentPadding: EdgeInsets.only(left: 12.0, right: 12.0, top: 8.0, bottom: 8.0)
-                  )
-      )
-  );
+          builder: (context, searchType) => TextField(
+              controller: controller,
+              onChanged: (text) => interactor.search(text),
+              style: TextStyle(fontSize: 18.0, color: Colors.black),
+              autofocus: true,
+              decoration: InputDecoration(
+                  hintText: _hint(searchType),
+                  suffix:
+                      CircleIconButton(onPressed: () => interactor.clearText()),
+                  contentPadding: EdgeInsets.only(
+                      left: 12.0, right: 12.0, top: 8.0, bottom: 8.0))));
 
-  String _hint(searchType) => 'Введіть назву ' + (searchType == ScheduleType.GROUP ? 'групи' : 'викладача');
+  String _hint(searchType) =>
+      'Введіть назву ' +
+      (searchType == ScheduleType.GROUP ? 'групи' : 'викладача');
 }
 
 class Progress extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Expanded(flex: 3, child: Center(child: CircularProgressIndicator()));
+  Widget build(BuildContext context) =>
+      Expanded(flex: 3, child: Center(child: CircularProgressIndicator()));
 }
 
 class SuggestionListView extends StatelessWidget {
   final List<String> suggestions;
   final SearchInteractor interactor;
+
   SuggestionListView(this.suggestions, this.interactor);
 
   @override
   Widget build(BuildContext context) => Expanded(child: _buildListView());
 
   ListView _buildListView() {
-    List<Widget> list = suggestions == null ? [] : suggestions.asMap()
-        .map((index, title) => MapEntry(index, _buildItem(index, title)))
-        .values
-        .map((suggestionItem) => Column(children: <Widget>[ suggestionItem, Divider(height: 1.0, color: Colors.grey[300]) ]))
-        .toList();
+    List<Widget> list = suggestions == null
+        ? []
+        : suggestions
+            .asMap()
+            .map((index, title) => MapEntry(index, _buildItem(index, title)))
+            .values
+            .map((suggestionItem) => Column(children: <Widget>[
+                  suggestionItem,
+                  Divider(height: 1.0, color: Colors.grey[300])
+                ]))
+            .toList();
     return ListView(children: list);
   }
 
-  Widget _buildItem(int index, String title) => StoreConnector<SearchState, ListItem>(
-      converter: (store) => store.state.selectedItem,
-      builder: (context, selectedItem) => _buildSuggestion(selectedItem, index, title)
-  );
+  Widget _buildItem(int index, String title) =>
+      StoreConnector<SearchState, ListItem>(
+          converter: (store) => store.state.selectedItem,
+          builder: (context, selectedItem) =>
+              _buildSuggestion(selectedItem, index, title));
 
-  SuggestionItem _buildSuggestion(ListItem selectedItem, int index, String title) =>
-      selectedItem == null || selectedItem.position == null || selectedItem.position != index ?
-    SuggestionItem(index, title, onClick: (index, text) => interactor.tapOnItem(ListItem(index, text))) :
-    SuggestionItem(index, title, onClick: (index, text) =>
-        interactor.tapOnItem(ListItem(index, text)), backgroundColor: Colors.blue, textColor: Colors.white);
+  SuggestionItem _buildSuggestion(
+          ListItem selectedItem, int index, String title) =>
+      selectedItem == null ||
+              selectedItem.position == null ||
+              selectedItem.position != index
+          ? SuggestionItem(index, title,
+              onClick: (index, text) =>
+                  interactor.tapOnItem(ListItem(index, text)))
+          : SuggestionItem(index, title,
+              onClick: (index, text) =>
+                  interactor.tapOnItem(ListItem(index, text)),
+              backgroundColor: Colors.blue,
+              textColor: Colors.white);
 }
 
 typedef void OnClick(int index, String text);
+
 class SuggestionItem extends StatelessWidget {
   final int index;
   final String text;
@@ -165,13 +192,19 @@ class SuggestionItem extends StatelessWidget {
   final Color backgroundColor;
   final OnClick onClick;
 
-  const SuggestionItem(this.index, this.text, {this.onClick, this.textColor, this.backgroundColor, Key key}) : super(key: key);
+  const SuggestionItem(this.index, this.text,
+      {this.onClick, this.textColor, this.backgroundColor, Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) => Container(
-    child: ListTile(title: Text(text, style: TextStyle(color: textColor)), onTap: () { onClick(index, text); }),
-    decoration: BoxDecoration(color: backgroundColor),
-  );
+        child: ListTile(
+            title: Text(text, style: TextStyle(color: textColor)),
+            onTap: () {
+              onClick(index, text);
+            }),
+        decoration: BoxDecoration(color: backgroundColor),
+      );
 }
 
 class CircleIconButton extends StatelessWidget {
@@ -190,8 +223,13 @@ class CircleIconButton extends StatelessWidget {
           child: Stack(
             alignment: Alignment(1.0, 1.0), // all centered
             children: <Widget>[
-              Container(width: size, height: size, decoration: BoxDecoration(shape: BoxShape.circle)),
-              Icon(icon, size: size * 0.8, color: Theme.of(context).accentColor) // 60% width for icon
+              Container(
+                  width: size,
+                  height: size,
+                  decoration: BoxDecoration(shape: BoxShape.circle)),
+              Icon(icon,
+                  size: size * 0.8,
+                  color: Theme.of(context).accentColor) // 60% width for icon
             ],
           )));
 }
@@ -205,7 +243,8 @@ class SearchTypeInfo extends StatefulWidget {
   AnimationController animationController() => searchType.translationController;
 }
 
-class SearchTypeState extends State<SearchTypeInfo> with SingleTickerProviderStateMixin {
+class SearchTypeState extends State<SearchTypeInfo>
+    with SingleTickerProviderStateMixin {
   Animation<double> translationAnimation;
   AnimationController translationController;
 
@@ -216,11 +255,13 @@ class SearchTypeState extends State<SearchTypeInfo> with SingleTickerProviderSta
   }
 
   void _initAnim() {
-    translationController = AnimationController(duration: const Duration(milliseconds: 150), vsync: this);
-    translationAnimation = Tween(begin: -0.1, end: 0.0).animate(translationController)
-      ..addListener(() {
-        setState(() {});
-      });
+    translationController = AnimationController(
+        duration: const Duration(milliseconds: 150), vsync: this);
+    translationAnimation =
+        Tween(begin: -0.1, end: 0.0).animate(translationController)
+          ..addListener(() {
+            setState(() {});
+          });
     translationController.forward();
   }
 
@@ -228,19 +269,19 @@ class SearchTypeState extends State<SearchTypeInfo> with SingleTickerProviderSta
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var animatedBuilder = AnimatedBuilder(
-          animation: translationController,
-          builder: (BuildContext context, Widget child) => Transform(
-              transform: Matrix4.translationValues(0.0, translationAnimation.value * height, 0.0),
-              child: StoreConnector<SearchState, String>(
-                  converter: (store) => store.state.searchType == ScheduleType.GROUP ? 'групи' : 'викладача',
-                  builder: (context, text) => AnimatedOpacity(
-                      opacity: translationAnimation.value > -0.03 ? 1.0 : 0.0,
-                      duration: Duration(milliseconds: 150),
-                      child: Text(text)
-                  )
-              )
-          )
-    );
+        animation: translationController,
+        builder: (BuildContext context, Widget child) => Transform(
+            transform: Matrix4.translationValues(
+                0.0, translationAnimation.value * height, 0.0),
+            child: StoreConnector<SearchState, String>(
+                converter: (store) =>
+                    store.state.searchType == ScheduleType.GROUP
+                        ? 'групи'
+                        : 'викладача',
+                builder: (context, text) => AnimatedOpacity(
+                    opacity: translationAnimation.value > -0.03 ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 150),
+                    child: Text(text)))));
     return animatedBuilder;
   }
 
